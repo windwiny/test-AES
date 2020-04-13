43eda983255864eca904dd658cbdb0c33c9450ff
windwiny <windwiny.ubt@gmail.com>
2020-04-14 00:20:33 +0800

    colorize cmd

--- test.rb
+++ test.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require 'shellwords'
 require 'openssl'
 
@@ -19,6 +21,45 @@ if !File.file? %{#{__dir__}/goenc}
   system "go build -o goenc goenc.go"
 end
 
+
+
+
+class String
+  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+
+  def blue
+    colorize(34)
+  end
+
+  def pink
+    colorize(35)
+  end
+
+  def light_blue
+    colorize(36)
+  end
+end
+def color_puts cmd
+  puts cmd.light_blue
+end
+
+
+
 %w{AES}.each do |cipher|
     %w{CFB CTR OFB}.each do |mode|
         STDERR.puts "---------- #{cipher} --- #{mode} -----------------------------"
@@ -28,26 +69,24 @@ end
         key = key.b.split(//).map{|e| format "%02X" % e.ord }.join
         iv  = iv.b.split(//).map{|e| format "%02X" % e.ord }.join
 
-        puts '-- ruby --'
         c1=%{ruby #{__dir__}/rubyenc.rb --enc #{cipher} --mode #{mode} -e --key #{key} --iv #{iv} |}
         c2=%{ruby #{__dir__}/rubyenc.rb --enc #{cipher} --mode #{mode} -d --key #{key} --iv #{iv} |} if $reverse
         cmd =  %{ cat '#{$file}' | #{c1} #{c2} md5sum - }
-        puts cmd
+        color_puts cmd
         system cmd
 
-        puts '-- golang --'
         c1=%{#{__dir__}/goenc --enc #{cipher} --mode #{mode} -e=true --key #{key} --iv #{iv} |}
         c2=%{#{__dir__}/goenc --enc #{cipher} --mode #{mode} -e=false --key #{key} --iv #{iv} |} if $reverse
         cmd =  %{ cat '#{$file}' | #{c1} #{c2} md5sum - }
-        puts cmd
+        color_puts cmd
         system cmd
 
-        puts '-- openssl --'
         c1=%{openssl enc -#{cipher.downcase}-#{llen*8}-#{mode.downcase} -K #{key} -iv #{iv} |}
         c2=%{openssl enc -d -#{cipher.downcase}-#{llen*8}-#{mode.downcase} -K #{key} -iv #{iv} |} if $reverse
         cmd =  %{ cat '#{$file}' | #{c1} #{c2} md5sum - }
-        puts cmd
+        color_puts cmd
         system cmd
 
     end
 end
+
